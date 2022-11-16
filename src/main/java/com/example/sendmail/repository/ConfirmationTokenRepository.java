27295b41feb90c33273fb5358c6aceedc79bc567
manh12a5 <manh12a5@gmail.com>
2022-11-16 10:35:45 +0700

    Resend token after registration

--- src/main/java/com/example/sendmail/repository/ConfirmationTokenRepository.java
+++ src/main/java/com/example/sendmail/repository/ConfirmationTokenRepository.java
@@ -2,6 +2,7 @@ package com.example.sendmail.repository;
 
 import com.example.sendmail.appuser.ConfirmationToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,4 +10,8 @@ import java.util.Optional;
 @Repository
 public interface ConfirmationTokenRepository extends JpaRepository<ConfirmationToken, Long> {
     Optional<ConfirmationToken> findByToken(String token);
+
+    @Query(value = "SELECT * FROM confirmation_token INNER JOIN app_user ON app_user.id = confirmation_token.user_id",
+            nativeQuery = true)
+    Optional<ConfirmationToken> findByAppUserId(Long id);
 }
